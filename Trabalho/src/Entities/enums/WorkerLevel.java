f6f309843a133edeee112634c4641a79a521687f
Victorgsp <victorgabrielpimenta52@gmail.com>
2023-09-12 14:17:25 -0300

    deletando todas as pastas

--- Trabalho/src/Entities/enums/WorkerLevel.java
+++
@@ -1,9 +0,0 @@
-package Entities.enums;
-
-public enum WorkerLevel {
-
-    JUNIOR,
-    MID_LEVEL,
-    SENIOR;
-
-}
--- sei la/src/Entities/enums/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package Entities.enums;
-
-public enum OrderStatus {
-
-	PENDING_PAYMENT,
-	PROCESSING,
-	SHIPPED,
-	DELIVERED;
-}
